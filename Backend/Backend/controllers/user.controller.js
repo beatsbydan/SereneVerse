75eee33d77cc0500bb3123b6ca35d02bbf0eb43b
Ireoluwatobi <111185454+Ireoluwatobi@users.noreply.github.com>
2024-02-06 22:30:19 +0100

    wrote logic to update password

--- Backend/Backend/controllers/user.controller.js
+++ Backend/Backend/controllers/user.controller.js
@@ -1,29 +1,62 @@
 const AsyncHandler = require("express-async-handler");
 const status = require("http-status");
-const {User} = require("../models/user.model")
+const ForbiddenRequestError = require("../exceptions/forbidden.exception");
+const UnauthorizedRequestError = require("../exceptions/badRequest.exception");
+const { hashPassword, comparePassword } = require("../utils/hashing.utils");
+const { User } = require("../models/user.model");
 
-const getAllUsers = AsyncHandler(async(req,res,next)=>{
-    try {
-        const users = await User.find({})
+const getAllUsers = AsyncHandler(async (req, res, next) => {
+  try {
+    const users = await User.find({});
 
-        return res.status(status.OK).json({
-            status : "success",
-            statusCode : status.OK,
-            data : users
-        })
-    } catch (error) {
-        next(error)
-    }
-})
+    const sanitizedUsers = users.map((user) => {
+      user.hash = undefined;
+      user._v = undefined;
+      user.refreshToken = undefined;
+    });
+    return res.status(status.OK).json({
+      status: "success",
+      statusCode: status.OK,
+      data: sanitizedUsers,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
 
-const updatePassword = AsyncHandler(async(req,res,next)=>{
-    try {
-    } catch (error) {
-        next(error)
-    }
-})
+const updatePassword = AsyncHandler(async (req, res, next) => {
+  try {
+    const id = req.userId;
+    const { newPass, currPass } = req.body;
+
+    const user = await User.findById(id);
+    if (!user) throw new ForbiddenRequestError("User not found");
+
+    const validate = await comparePassword(user.hash, currPass);
+    if (!validate) throw new UnauthorizedRequestError("Password mismatch");
+
+    const hash = await hashPassword(newPass);
+    user.hash = hash;
+    await user.save();
+
+    const sanitizedUser = {
+      ...user,
+      hash: undefined,
+      password: newPass,
+      _v: undefined,
+      refreshToken: undefined,
+    };
+    return res.status(status.CREATED).json({
+      status: "success",
+      statusCode: status.CREATED,
+      data: sanitizedUser,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = {
-    getAllUsers,
-    updatePassword
-}
+  getAllUsers,
+  updatePassword,
+};
--- Backend/Backend/routes/user.route.js
+++ Backend/Backend/routes/user.route.js
@@ -1,13 +1,15 @@
-const {Router} = require("express")
+const { Router } = require("express");
 
-const { getAllUsers, updatePassword } = require("../controllers/user.controller")
-const authMiddleware = require("../middlewares/auth.middleware")
+const {
+  getAllUsers,
+  updatePassword,
+} = require("../controllers/user.controller");
+const authMiddleware = require("../middlewares/auth.middleware");
 
-const userRouter = Router()
+const userRouter = Router();
 
-userRouter.route("/update").post(authMiddleware, updatePassword)
+userRouter.route("/update").post(authMiddleware, updatePassword);
 
-userRouter.route("/").get(authMiddleware, getAllUsers)
+userRouter.route("/").get(authMiddleware, getAllUsers);
 
-module.exports = userRouter
-
+module.exports = userRouter;
