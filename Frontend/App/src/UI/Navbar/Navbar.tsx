7907b2c6ff6cdabb63368ed8ee26686788b7de28
Onyeachonam Daniel <danielonyeachonam2020@gmail.com>
2024-02-20 10:30:23 +0100

    Navbar component started

---
+++ Frontend/App/src/UI/Navbar/Navbar.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+    return (
+        <div className='navBar'>
+            <div className=""></div>
+            <div className=""></div>
+            <div className=""></div>
+            <div className=""></div>
+        </div>
+    )
+}
+
+export default Navbar
