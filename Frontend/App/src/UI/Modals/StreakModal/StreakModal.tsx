27444deb1f6d4ebc39a25d2c95135c5525402fbc
Onyeachonam Daniel <danielonyeachonam2020@gmail.com>
2024-02-25 22:12:36 +0100

    update

--- Frontend/App/src/UI/Modals/StreakModal/StreakModal.tsx
+++ Frontend/App/src/UI/Modals/StreakModal/StreakModal.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { ReactNode } from 'react'
 
-const StreakModal:React.FC = () => {
+const StreakModal:React.FC<{children: ReactNode}> = (props) => {
   return (
     <div className='streakModal'>
       <div className="streakModal--bg"></div>
+      {props.children}
     </div>
   )
 }
