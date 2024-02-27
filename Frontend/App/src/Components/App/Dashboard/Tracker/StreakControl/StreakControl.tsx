4b565d815b1e0f8f5745e51a739c2a32fbfdb8df
Onyeachonam Daniel <danielonyeachonam2020@gmail.com>
2024-02-27 13:31:02 +0100

    updates

--- Frontend/App/src/Components/App/Dashboard/Tracker/StreakControl/StreakControl.tsx
+++ Frontend/App/src/Components/App/Dashboard/Tracker/StreakControl/StreakControl.tsx
@@ -1,54 +1,65 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from '../../../../../UI/Button/AppButton/Button'
 import start from '../../../../../Assets/icons/start-streak.svg'
 import end from '../../../../../Assets/icons/end-streak.svg'
 import restart from '../../../../../Assets/icons/restart-streak.svg'
 import streak from '../../../../../Assets/icons/streak.svg'
 import './StreakControl.css'
-
-const StreakElements = [
-    {
-        type: 'Start',
-        desc: 'Start a new abstinence streak today!',
-        url: start,
-        actionHandler: ()=>{}
-    },
-    {
-        type: 'End',
-        desc: 'End an existing abstinence streak.',
-        url: end,
-        actionHandler: ()=>{}
-    },
-    {
-        type: 'Restart',
-        desc: 'Restart a new abstinence streak that has ended.',
-        url: restart,
-        actionHandler: ()=>{}
-    },
-]
-
+import StreakStart from '../../../../../UI/Modals/StreakModal/StreakModal'
 
 const StreakControl:React.FC = () => {
+    const [isOpen, setIsOpen] = useState(true);
+    const changeIsOpen = () =>{
+        setIsOpen(!isOpen)
+    }
+
+    const StreakElements = [
+        {
+            type: 'Start',
+            desc: 'Start a new abstinence streak today!',
+            url: start,
+            actionHandler: ()=>{}
+        },
+        {
+            type: 'End',
+            desc: 'End an existing abstinence streak.',
+            url: end,
+            actionHandler: ()=>{}
+        },
+        {
+            type: 'Restart',
+            desc: 'Restart a new abstinence streak that has ended.',
+            url: restart,
+            actionHandler: ()=>{}
+        },
+    ]
+    
     return (
-        <ul className='streakControl'>
-            {
-                StreakElements.map((value, index)=>{
-                    return (
-                        <li key={index} className='streakSetting'>
-                            <div className="streakSettingDesc">
-                                <img src={streak} alt="streak" />
-                                <h4>{value.desc}</h4>
-                                <img src={value.url} alt={value.type} />
-                            </div>
-                            <Button
-                                text={`${value.type} streak`}
-                                actionHandler={value.actionHandler}
-                            />
-                        </li>
-                    )
-                })
-            }
-        </ul>
+        <>
+            <ul className='streakControl'>
+                {
+                    StreakElements.map((value, index)=>{
+                        return (
+                            <li key={index} className='streakSetting'>
+                                <div className="streakSettingDesc">
+                                    <img src={streak} alt="streak" />
+                                    <h4>{value.desc}</h4>
+                                    <img src={value.url} alt={value.type} />
+                                </div>
+                                <Button
+                                    text={`${value.type} streak`}
+                                    actionHandler={value.actionHandler}
+                                />
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+            {<StreakStart
+                isOpen={isOpen}
+                closeModal={changeIsOpen}
+            />}
+        </>
     )
 }
 
--- Frontend/App/src/UI/Modals/StreakModal/StreakModal.tsx
+++ Frontend/App/src/UI/Modals/StreakModal/StreakModal.tsx
@@ -1,12 +1,66 @@
-import React, { ReactNode } from 'react'
+import React, { ReactNode, useEffect, useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import ReactDOM from 'react-dom'
+import Button from '../../Button/AppButton/Button'
 
-const StreakModal:React.FC<{children: ReactNode}> = (props) => {
-  return (
-    <div className='streakModal'>
-      <div className="streakModal--bg"></div>
-      {props.children}
+const StreakUI:React.FC<{}> = (props) => {
+  const [streakName, setStreakName] = useState('')
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setStreakName(e.target.value);
+  }
+
+  return(
+    <div className="streakUI">
+      <div className="streakUi--head"></div>
+      <input 
+        type="text" 
+        name='streakName' 
+        placeholder="Enter what you're abstaining from" 
+        onChange={handleChange}
+      />
+      <Button
+        text="Start streak"
+        actionHandler={()=>{}}
+      />
     </div>
   )
 }
 
-export default StreakModal
+const StreakModal:React.FC<{children: ReactNode, isOpen: boolean, closeModal: ()=>void}> = (props) => {
+  const [modalElement] = useState(document.createElement('div'))
+    
+    useEffect(()=>{
+        document.body.appendChild(modalElement)
+
+        return () => {
+            document.body.removeChild(modalElement)
+        }
+
+    },[modalElement])
+
+    return props.isOpen ?
+        ReactDOM.createPortal(
+            <React.Fragment>
+              <div className="streakModal--bg" onClick={props.closeModal}></div>
+              {props.children}
+            </React.Fragment>
+            ,
+            modalElement
+        )
+        :
+        null
+}
+
+const StreakStart:React.FC<
+  {
+    isOpen: boolean, 
+    closeModal: ()=> void
+  }> = (props) => {
+    return(
+      <StreakModal isOpen={props.isOpen} closeModal={props.closeModal}>
+        <StreakUI/>
+      </StreakModal>
+    )
+}
+
+export default StreakStart
